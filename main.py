760e8eae55dd55b848e0809cfba84d2ab304b4ac
Lucas Leal <lucas.sjleal@gmail.com>
2024-09-06 23:58:35 -0300

    configurações e elementos

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import flet as ft
 
 def main(page: ft.Page):
-    
-    page.update()
+
+    page.window.center()
+    page.theme_mode=ft.ThemeMode.DARK
+    page.spacing=0
+    page.padding=0
+    page.window.width = 400
+    page.window.height = 400
+
+    titulo = ft.Text(value='Horas extras', size=30, color=ft.colors.WHITE70, weight=ft.FontWeight.W_600)
+    txt_salario = ft.TextField()
+
+    container = ft.Container(
+        expand=True,
+        content=ft.Column(
+            controls=[
+                titulo,
+                txt_salario
+            ]
+        ),
+        alignment=ft.alignment.center
+    )
+
+    page.add(container)
 
 ft.app(target=main)
 
